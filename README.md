51dc0bb31c4d8d1c84a5df15b2d16ccddb10f9f3
Hrishikesh Kadam <hrkadam.92@gmail.com>
2022-11-23 23:25:35 +0530

    flutter create
    flutter create \
      --org "dev.hrishikesh_kadam.flutter" \
      --project-name "space_data_explorer" .

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # space_data_explorer
+
+An app to explore space data from various sources.
---
+++ android/app/src/main/kotlin/dev/hrishikesh_kadam/flutter/space_data_explorer/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.hrishikesh_kadam.flutter.space_data_explorer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
