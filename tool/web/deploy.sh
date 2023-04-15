316f7ce1defa36afb1f28fb8c52f4edeac6a84dd
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-04-15 18:16:41 +0530

    8th commit

--- tool/web/deploy.sh
+++ tool/web/deploy.sh
@@ -9,16 +9,17 @@ BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 if [[ ! -d ./secrets ]]; then
   if [[ -n $FIREBASE_SERVICE_ACCOUNT_CONTRI ]]; then
     echo "$FIREBASE_SERVICE_ACCOUNT_CONTRI" > /tmp/firebase-service-account-contri.json
+    GOOGLE_APPLICATION_CREDENTIALS="/tmp/firebase-service-account-contri.json"
   fi
   FIREBASE_PROJECT_ID="${APP_NAME_KEBAB_CASE}-contri"
 elif [[ $BRANCH == "stag" ]]; then
-  export GOOGLE_APPLICATION_CREDENTIALS="./secrets/web/firebase-service-account-stag.json"
+  GOOGLE_APPLICATION_CREDENTIALS="./secrets/web/firebase-service-account-stag.json"
   FIREBASE_PROJECT_ID="${APP_NAME_KEBAB_CASE}-stag"
 elif [[ $BRANCH == "prod" ]]; then
-  export GOOGLE_APPLICATION_CREDENTIALS="./secrets/web/firebase-service-account-prod.json"
+  GOOGLE_APPLICATION_CREDENTIALS="./secrets/web/firebase-service-account-prod.json"
   FIREBASE_PROJECT_ID="${APP_NAME_KEBAB_CASE}"
 else
-  export GOOGLE_APPLICATION_CREDENTIALS="./secrets/web/firebase-service-account-dev.json"
+  GOOGLE_APPLICATION_CREDENTIALS="./secrets/web/firebase-service-account-dev.json"
   FIREBASE_PROJECT_ID="${APP_NAME_KEBAB_CASE}-dev"
 fi
 
@@ -32,11 +33,17 @@ else
   FIREBASE_CHANNEL_ID="live"
 fi
 
-firebase use $FIREBASE_PROJECT_ID
-if [[ $FIREBASE_CHANNEL_ID == "live" ]]; then
-  firebase deploy --only hosting
+if [[ $GITHUB_ACTION ]]; then
+  FIREBASE="GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS firebase"
 else
-  firebase hosting:channel:deploy "$FIREBASE_CHANNEL_ID" \
+  FIREBASE="firebase"
+fi
+
+$FIREBASE use $FIREBASE_PROJECT_ID
+if [[ $FIREBASE_CHANNEL_ID == "live" ]]; then
+  $FIREBASE deploy --only hosting
+else
+  $FIREBASE hosting:channel:deploy "$FIREBASE_CHANNEL_ID" \
     --expires 30d
 fi
 
