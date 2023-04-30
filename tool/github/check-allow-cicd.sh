d929606cfc518834f7348b6f435ecc741f647af6
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-04-30 17:38:19 +0530

    Add reference links for check-allow-cicd.sh

--- tool/github/check-allow-cicd.sh
+++ tool/github/check-allow-cicd.sh
@@ -8,6 +8,8 @@ PRINT_DEBUG_LOG=1
 if [[ $GITHUB_TRIGGERING_ACTOR != "dependabot[bot]" ]]; then
   ALLOW_CICD=true
 elif [[ $GITHUB_EVENT_NAME == "pull_request" ]]; then
+  # https://docs.github.com/en/webhooks-and-events/webhooks/webhook-events-and-payloads?actionType=opened#pull_request
+  # https://docs.github.com/en/graphql/reference/enums#commentauthorassociation
   AUTHOR_ASSOCIATION=$(jq .pull_request.author_association "$GITHUB_EVENT_PATH")
   debug_log "AUTHOR_ASSOCIATION=$AUTHOR_ASSOCIATION"
   if [[ $AUTHOR_ASSOCIATION == "OWNER" \
