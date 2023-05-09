6e17e5a0a2a4e49c3a6f93d05a6c936f5174cf4e
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-05-09 17:00:54 +0530

    Add TODO for openssl prerequisite

--- tool/prerequisite.sh
+++ tool/prerequisite.sh
@@ -124,6 +124,8 @@ if [[ ! -x $(command -v yq) ]]; then
   yq --version
 fi
 
+# TODO(hrishikesh-kadam): Check openssl on local macOS and Windows
+
 if [[ ! -s $BUNDLETOOL_PATH ]]; then
   ./tool/android/install-bundletool.sh
 fi
