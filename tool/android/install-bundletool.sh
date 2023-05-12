fc31603e24e465c3e8889f6f6164c4cf2bae1db1
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-05-12 22:53:02 +0530

    Print out VERSION in bundletool script

--- tool/android/install-bundletool.sh
+++ tool/android/install-bundletool.sh
@@ -3,9 +3,10 @@
 set -e -o pipefail
 
 VERSION=$( \
-  curl -s "https://api.github.com/repos/google/bundletool/releases/latest" \
+  curl -sL "https://api.github.com/repos/google/bundletool/releases/latest" \
     | jq -r .name
 )
+echo "VERSION=$VERSION"
 BUNDLETOOL_PATH="$ANDROID_HOME/bundletool-all.jar"
 curl -o "$BUNDLETOOL_PATH" \
   -sL "https://github.com/google/bundletool/releases/download/$VERSION/bundletool-all-$VERSION.jar"
