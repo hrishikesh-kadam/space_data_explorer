a62fff6681d48cadfdb7cf558004b7bfb74a0751
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-06-13 19:50:11 +0530

    Check line endings only on Windows

--- tool/ci.sh
+++ tool/ci.sh
@@ -4,7 +4,9 @@ set -e -o pipefail
 
 source ./tool/shell/logs-env.sh
 
-./tool/check-line-endings.sh
+if [[ $(uname -s) =~ ^"MINGW" ]]; then
+  ./tool/check-line-endings.sh
+fi
 
 ./tool/shell/analyze.sh
 
