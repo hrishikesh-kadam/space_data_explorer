a33269516de0f5be42c6ee3f2f0928a5fffe8592
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-06-13 22:00:58 +0530

    Fix log-env scripts sequence

--- tool/shell/logs-env.sh
+++ tool/shell/logs-env.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+source ./tool/shell/logs-env-posix.sh
+source ./tool/shell/logs-env-bash.sh
+
 if [ -z ${-%*e*} ]; then PARENT_ERREXIT=true; else PARENT_ERREXIT=false; fi
 if shopt -qo pipefail; then PARENT_PIPEFAIL=true; else PARENT_PIPEFAIL=false; fi
 
 set -e -o pipefail
 
-source ./tool/shell/logs-env-posix.sh
-source ./tool/shell/logs-env-bash.sh
 PRINT_DEBUG_LOG=1
 PRINT_INFO_LOG=1
 
