d6814f7d69421047745308aff8e3c9cad5935e9b
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-06-23 15:42:03 +0530

    Add comments in isFlutterTest()

--- lib/utility/utility_non_web.dart
+++ lib/utility/utility_non_web.dart
@@ -3,10 +3,14 @@ import 'dart:io';
 bool isAndroid = Platform.isAndroid;
 
 bool isFlutterTest() {
+  // For Unit and Widget Test run as `flutter test` or `flutter run`,
+  // `FLUTTER_TEST` is set to true
   bool flutterTest = Platform.environment.containsKey('FLUTTER_TEST');
   if (flutterTest) {
     return flutterTest;
   }
+  // For Integration Test run as `flutter test` or `flutter drive`,
+  // `FLUTTER_TEST` needs to be passed as --dart-define="FLUTTER_TEST=true"
   flutterTest = const bool.fromEnvironment('FLUTTER_TEST');
   return flutterTest;
 }
