f61cb849cc9fd39e625a49d06d0e0b27fddf8959
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-07-20 17:24:19 +0530

    Format code

--- integration_test/hydrated_bloc.dart
+++ integration_test/hydrated_bloc.dart
@@ -4,9 +4,9 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
+@GenerateNiceMocks([MockSpec<Storage>()])
 import 'hydrated_bloc.mocks.dart';
 
-@GenerateNiceMocks([MockSpec<Storage>()])
 void mockHydratedBloc() {
   MockStorage storage = MockStorage();
   when(storage.write(any, any)).thenAnswer((_) async {});
