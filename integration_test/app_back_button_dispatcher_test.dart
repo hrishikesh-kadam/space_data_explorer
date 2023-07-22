be7d884f49ad5001f496eb9d9cfed5a7bc2a7f1b
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-07-22 12:07:42 +0530

    Delete integration_test/app_back_button_dispatcher_test.dart

--- integration_test/app_back_button_dispatcher_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:integration_test/integration_test.dart';
-
-import '../test/widget_test/config/app_back_button_dispatcher_test.dart';
-
-void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-  appBackButtonDispatcherTest();
-}
