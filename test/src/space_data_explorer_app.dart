631d7310f6b3fecff705e2b07fb9c899775b34fd
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-07-22 12:28:32 +0530

    Add actions after github.com/flutter/flutter#102469 is resolved

--- test/src/space_data_explorer_app.dart
+++ test/src/space_data_explorer_app.dart
@@ -6,7 +6,8 @@ import 'globals.dart';
 
 /*
  * TODO(hrishikesh-kadam): Check if the following issue is resolved.
- * https://github.com/flutter/flutter/issues/102469 
+ * https://github.com/flutter/flutter/issues/102469
+ * Once that is resolved `rm integration_test/test` 
  */
 Future<void> pumpApp(
   WidgetTester tester, {
