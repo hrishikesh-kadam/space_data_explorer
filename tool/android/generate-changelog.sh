b829e0e7c5280a6793f2b6514ca6634d7019b58b
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-07-24 18:54:52 +0530

    Fix Android Changelog 500 limit
    Error message from fastlane:
    [!] Google Api Error: Invalid request - The release created has notes in language en-US with length 617, which is too long (max: 500).

--- tool/android/generate-changelog.sh
+++ tool/android/generate-changelog.sh
@@ -10,18 +10,22 @@ FLAVOR_ENV=$1
 VERSION_CODE=$2
 VERSION_NAME=$3
 
-if [[ $FLAVOR_ENV == "prod" ]]; then
-  exit 0
-fi
-
 CHANGELOG_DIRECTORY="./android/fastlane/$FLAVOR_ENV/metadata/android/en-US/changelogs"
 mkdir -p "$CHANGELOG_DIRECTORY"
 CHANGELOG_FILE="$CHANGELOG_DIRECTORY/default.txt"
-printf "" > "$CHANGELOG_FILE"
 
-if [[ -n $VERSION_CODE && -n $VERSION_NAME ]]; then
-  echo "# $VERSION_CODE ($VERSION_NAME)" > "$CHANGELOG_FILE"
+if [[ $FLAVOR_ENV == "dev" || $FLAVOR_ENV == "stag" ]]; then
+  printf "" > "$CHANGELOG_FILE"
+  if [[ -n $VERSION_CODE && -n $VERSION_NAME ]]; then
+    echo "# $VERSION_CODE ($VERSION_NAME)" > "$CHANGELOG_FILE"
+  fi
+  CHANGELOG_COMMITS=$(git log -10 --format="- %s")
+  echo "$CHANGELOG_COMMITS" >> "$CHANGELOG_FILE"
 fi
 
-CHANGELOG_COMMITS=$(git log -10 --format="- %s")
-echo "$CHANGELOG_COMMITS" >> "$CHANGELOG_FILE"
+if (( $(wc -c < "$CHANGELOG_FILE") > 500 )); then
+  TRUNCATED_CHANGELOG_FILE="$CHANGELOG_DIRECTORY/default_truncated.txt"
+  head -c 497 "$CHANGELOG_FILE" > "$TRUNCATED_CHANGELOG_FILE"
+  printf "..." >> "$TRUNCATED_CHANGELOG_FILE"
+  mv "$TRUNCATED_CHANGELOG_FILE" "$CHANGELOG_FILE"
+fi
