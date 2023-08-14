e16059217e6db22303bc9227956557b7bc0925ae
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-08-15 05:05:59 +0530

    Add ValueRangeFilterWidget Painting Test

---
+++ test/widget_test/nasa/cad/query/painting/distance_filter_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hrk_flutter_test_batteries/hrk_flutter_test_batteries.dart';
+import 'package:hrk_nasa_apis/hrk_nasa_apis.dart';
+
+import 'package:space_data_explorer/nasa/cad/cad_route.dart';
+import 'package:space_data_explorer/nasa/cad/cad_screen.dart';
+import 'package:space_data_explorer/widgets/value_range_filter_widget.dart';
+import '../../../../../constants/dimensions.dart';
+import '../../../../../src/nasa/cad/cad_route.dart';
+import '../../../../../src/nasa/cad/query/distance_filter.dart';
+
+void main() {
+  group(
+      '$CadRoute ${ValueRangeFilterWidget<double, DistanceUnit>} Painting Test',
+      () {
+    testWidgets('Doesn\'t Overflow ${Dimensions.galaxyFoldPortraitWidth}',
+        (tester) async {
+      disableOverflowError();
+      tester.view.setLogicalSize(width: Dimensions.galaxyFoldPortraitWidth);
+      await pumpCadRouteAsInitialLocation(tester);
+      tester.expectNoOverflow(of: distanceFilterWidgetFinder);
+      for (final filter in DistanceFilter.values) {
+        for (final unit in CadScreen.distanceFilterUnits) {
+          await tapUnitDropdown(tester, filter);
+          tester.expectNoOverflow(of: distanceFilterWidgetFinder);
+          await tapUnitDropdownItem(tester, filter, unit);
+          tester.expectNoOverflow(of: distanceFilterWidgetFinder);
+        }
+      }
+    });
+  });
+}
