aae2d58e3a8a7b04d87e181f83307de6cea5c443
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-09-10 23:28:16 +0530

    Add references to date patterns and symbols

--- lib/route/settings/date_format_pattern.dart
+++ lib/route/settings/date_format_pattern.dart
@@ -2,6 +2,12 @@
 
 import 'package:json_annotation/json_annotation.dart';
 
+// References:
+// - https://github.com/dart-lang/i18n/blob/main/pkgs/intl/lib/src/data/dates
+// - https://github.com/dart-lang/i18n/blob/main/pkgs/intl/lib/date_time_patterns.dart
+// - https://github.com/dart-lang/i18n/blob/main/pkgs/intl/lib/date_symbol_data_local.dart
+// - https://github.com/flutter/flutter/blob/master/packages/flutter_localizations/lib/src/l10n/generated_date_localizations.dart
+// - https://github.com/unicode-org/cldr-json/blob/main/cldr-json/cldr-dates-full/main/en-IN/ca-generic.json
 enum DateFormatPattern {
   @JsonValue('yMd')
   yMd(pattern: 'yMd'),
