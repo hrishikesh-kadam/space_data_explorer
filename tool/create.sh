4c26cba7934a78e06f16ddd354330a81148872b2
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-10-19 19:34:07 +0530

    Fix font updation

--- tool/create.sh
+++ tool/create.sh
@@ -6,15 +6,16 @@ if [[ $LOGS_ENV_SOURCED != "true" ]]; then
   source ./tool/shell/logs-env.sh
 fi
 
+flutter create . --org "dev.hrishikesh_kadam.flutter"
+
 # For Golden File Test
 # Keep checking for any better solution for updating icons
 # https://github.com/flutter/flutter/wiki/Updating-Material-Design-Fonts-&-Icons
+# flutter create downloads assets of material_fonts if the directory is absent
 cp "$FLUTTER_ROOT/bin/cache/artifacts/material_fonts/Roboto-Regular.ttf" \
   "assets/fonts/Roboto"
 cp "$FLUTTER_ROOT/bin/cache/artifacts/material_fonts/MaterialIcons-Regular.otf" \
   "assets/fonts/MaterialIcons"
-
-flutter create . --org "dev.hrishikesh_kadam.flutter"
 
 # if ! jq -e '. == {}' lib/l10n/unstranslated-messages.json &> /dev/null; then
 #   log_error_with_exit "Unstranslated messages found" 1
