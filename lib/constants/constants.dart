9ee6d4d9b94f7598bcb4310c374032f813e5e93f
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-10-26 11:18:12 +0530

    Start with v0.8.0

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -2,7 +2,7 @@ import 'package:recase/recase.dart';
 
 class Constants {
   static const String appName = 'space_data_explorer';
-  static const String version = '0.7.0';
+  static const String version = '0.8.0';
   static const authorUsername = 'hrishikesh-kadam';
   static final Uri linktreeUrl = Uri.https(
     'linktr.ee',
