9611fd6a8a3ec6165c0057d1f680d8da478751eb
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-11-18 21:57:16 +0530

    [ios] Add tool/ios/build.sh

---
+++ tool/ios/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+FLAVOR_ENV=$(./tool/get-flavor-env.sh)
+
+flutter build ipa \
+  --flavor "$FLAVOR_ENV" \
+  --dart-define="FLAVOR_ENV=$FLAVOR_ENV" \
+  --export-options-plist="./ios/config/dev/ExportOptions.plist"
