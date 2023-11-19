7362366b9ff244cd62008ae976701801c0855cee
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-11-19 20:24:09 +0530

    Start with ios build on GitHub Actions Workflow

---
+++ tool/ios/ci.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+source ./tool/shell/logs-env.sh
+
+./tool/check-line-endings.sh
+
+./tool/shell/analyze.sh
+
+./tool/create.sh
+
+./tool/format-analyze.sh
+
+./tool/test.sh
+
+./tool/ios/build.sh
+
+git status -s
