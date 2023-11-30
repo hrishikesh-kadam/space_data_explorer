b0128c8e0c3d3536b6a5acc5f5e864218a05ab01
Hrishikesh Kadam <hrkadam.92@gmail.com>
2023-11-30 09:16:47 +0530

    Refactor tool/ios/download-metadata-screenshots.sh

--- tool/ios/download-metadata-screenshots.sh
+++ tool/ios/download-metadata-screenshots.sh
@@ -1,44 +1,43 @@
 #!/usr/bin/env bash
 
+# Arguments:
+#   $1 FLAVOR_ENV dev / stag / prod.
+
 set -e -o pipefail
 
-source ./tool/constants.sh
+FLAVOR_ENV=${1:?"Missing argument \$1 FLAVOR_ENV dev / stag / prod."}
 
 API_KEY_PATH_ARG=()
 if [[ -s ./secrets/.git ]]; then
   if [[ $GITHUB_ACTIONS == "true" ]]; then
-    API_KEY_PATH_ARG=(api_key_path:"../secrets/ios/app-store-connect/non-interactive.json")
+    API_KEY_PATH_ARG=(--api_key_path "../secrets/ios/app-store-connect/non-interactive.json")
   else
-    API_KEY_PATH_ARG=(api_key_path:"../secrets/ios/app-store-connect/interactive.json")
+    API_KEY_PATH_ARG=(--api_key_path "../secrets/ios/app-store-connect/interactive.json")
   fi
 fi
 
+APP_IDENTIFIER="$(./tool/ios/get-app-identifier.sh "$FLAVOR_ENV")"
+
 pushd ios &> /dev/null
 
-APP_IDENTIFIERS=(
-  "$APPLE_BUNDLE_ID.dev.release"
-  # "$APPLE_BUNDLE_ID.stag.release"
-  # "$APPLE_BUNDLE_ID"
-)
-FLAVOR_ENV=(
-  "dev"
-  # "stag"
-  # "prod"
-)
+USE_LIVE_VERSION_ARG=()
+if [[ $FLAVOR_ENV == "prod" ]]; then
+  USE_LIVE_VERSION_ARG=(--use_live_version true)
+fi
 
-for i in "${!APP_IDENTIFIERS[@]}"; do
-  bundle exec fastlane run upload_to_app_store download_metadata \
-    "${API_KEY_PATH_ARG[@]}" \
-    app_identifier:"${APP_IDENTIFIERS[i]}" \
-    metadata_path:"./fastlane/${FLAVOR_ENV[i]}/metadata" \
-    force:"true"
+bundle exec fastlane upload_to_app_store download_metadata \
+  "${API_KEY_PATH_ARG[@]}" \
+  --app_identifier "$APP_IDENTIFIER" \
+  --metadata_path "./fastlane/$FLAVOR_ENV/metadata" \
+  "${USE_LIVE_VERSION_ARG[@]}" \
+  --force true
 
-  # bundle exec fastlane run upload_to_app_store download_screenshots \
-  #   "${API_KEY_PATH_ARG[@]}" \
-  #   app_identifier:"${APP_IDENTIFIERS[i]}" \
-  #   screenshots_path:"./fastlane/${FLAVOR_ENV[i]}/screenshots" \
-  #   force:"true"
-done
+bundle exec fastlane upload_to_app_store download_screenshots \
+  "${API_KEY_PATH_ARG[@]}" \
+  --app_identifier "$APP_IDENTIFIER" \
+  --screenshots_path "./fastlane/$FLAVOR_ENV/screenshots" \
+  "${USE_LIVE_VERSION_ARG[@]}" \
+  --force true
 
 rm -rf ./fastlane/metadata ./fastlane/screenshots
 
