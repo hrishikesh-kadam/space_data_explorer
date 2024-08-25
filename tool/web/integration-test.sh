76ed87dc56c0fbcb41347f9352b9b7d332bfba0f
Hrishikesh Kadam <hrkadam.92@gmail.com>
2024-08-25 17:48:14 +0530

    Remove web-renderer html for flutter drive

--- tool/web/integration-test.sh
+++ tool/web/integration-test.sh
@@ -21,24 +21,11 @@ if [[ $GITHUB_ACTIONS == "true" ]] ; then
 else
   DEVICE="chrome"
 fi
-# TODO(hrishikesh-kadam): Keep checking this for stable updates
-# The `flutter drive` command needs `--web-renderer html` because: 
-# 1. Default web renderer is moving to canvaskit.  
-#    This change is after 3.22 announcement.
-# 2. Fix for flutter drive command with web renderer canvaskit is in 
-#    master channel but not in stable (3.24) at this time.  
-#    Couldn't know which commit in flutter/flutter resolves the issue.
-# References:
-# - https://github.com/flutter/flutter/issues/149826
-# - https://github.com/flutter/packages/pull/7115#issuecomment-2229569641
-# - https://github.com/flutter/packages/pull/7146
-# - https://github.com/flutter/flutter/issues/151869
 for TARGET_PATH in "${TARGET_PATHS[@]}"; do
   flutter drive \
     --dart-define="FLAVOR_ENV=$FLAVOR_ENV" \
     --dart-define="FLUTTER_TEST=true" \
     --driver test_driver/integration_test.dart \
     --target "$TARGET_PATH" \
-    --web-renderer html \
     -d $DEVICE
 done
